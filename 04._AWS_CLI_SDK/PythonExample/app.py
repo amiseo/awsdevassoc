27ae194e9aff8fff9d886dba9d909a5627487980
Nick Garner <nick@awsdev.guru>
2018-08-28 20:33:18 +0000

    20180828-2033 bug fix on python example

--- 04._AWS_CLI_SDK/PythonExample/app.py
+++ 04._AWS_CLI_SDK/PythonExample/app.py
@@ -1,6 +1,6 @@
 import boto3    
 
-ec2client = boto3.client('ec2', aws_access_key_id ='INSERT_KEY_ID', aws_secret_access_key='INSERT_SECRET', region_name='INSERT_REGION')
+ec2client = boto3.client('ec2', aws_access_key_id ='INSERT_ACCESS', aws_secret_access_key='INSERT_SECRET', region_name='INSERT_REGION')
 
 response = ec2client.describe_instances()
 
@@ -12,5 +12,6 @@ for reservation in response["Reservations"]:
         # The next line will print the entire dictionary object
         #print(instance)
         # The next line will print only the InstanceId and Public IP Address
-        print(instance["InstanceId"] + "\t" + instance["PublicIpAddress"])
+        if ("PublicIpAddress" in instance): 
+            print(instance["InstanceId"] + "\t" + instance["PublicIpAddress"])
 
